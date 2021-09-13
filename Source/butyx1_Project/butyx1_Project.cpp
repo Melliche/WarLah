c8636a084fd57c7a5522a3a8b0742a824b70625e
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2021-09-13 17:12:58 +0200

    move inputs

---
+++ Source/butyx1_Project/butyx1_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "butyx1_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, butyx1_Project, "butyx1_Project" );
---
+++ Source/butyx1_Project/butyx1_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "butyx1_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUTYX1_PROJECT_API Abutyx1_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
